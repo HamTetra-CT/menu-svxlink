c4b6ab3e0dc7448a20ad3cdca83effc8fbcb627a
Francisco (F4VSE) <kikosgc@users.noreply.github.com>
2024-04-06 21:40:09 +0200

    🆕 New menu option: View SVXLink Log

--- menutetra.sh
+++ menutetra.sh
@@ -8,17 +8,18 @@
 while : ; do
 
 escolha=$(whiptail --title "Menu de Gestão do Repetidor TETRA" --menu " >> Faça a sua escolha: " 25 80 11 \
-1 " Editar a configuração do repetidor (svxlink.conf) " \
-2 " Editar a configuração da tetra-contrib (TetraLogic.conf) " \
-3 " Editar o ficheiro de utilizadores CT (tetra_users.json) " \
-4 " Actualizadar o ficheiro de utilizadores CT (tetra_users.json) " \
-5 " Configurações audio (alsamixer) " \
-6 " Actualizadar svxlink + tetra-contrib /!\RISCO DE INCOMPATIBILIDADES/!\ " \
-7 " Parar svxlink " \
-8 " Reboot svxlink " \
-9 " Configurações da Raspberry Pi (raspi-config) " \
-10 " Reboot da Raspberry Pi " \
-11 " Sair do menu "  3>&1 1>&2 2>&3)
+1 " Consultar log SVXLink " \
+2 " Editar a configuração do repetidor (svxlink.conf) " \
+3 " Editar a configuração da tetra-contrib (TetraLogic.conf) " \
+4 " Editar o ficheiro de utilizadores CT (tetra_users.json) " \
+5 " Actualizadar o ficheiro de utilizadores CT (tetra_users.json) " \
+6 " Configurações audio (alsamixer) " \
+7 " Actualizadar SVXLink + tetra-contrib /!\RISCO DE INCOMPATIBILIDADES/!\ " \
+8 " Parar SVXLink " \
+9 " Reboot SVXLink " \
+10 " Configurações da Raspberry Pi (raspi-config) " \
+11 " Reboot da Raspberry Pi " \
+12 " Sair do menu "  3>&1 1>&2 2>&3)
 
 exitstatus=$?
 
@@ -38,20 +39,22 @@ fi
 case $escolha in
 
 1)
-sudo nano /etc/svxlink/svxlink.conf;;
+tail -f /var/log/svxlink;;
 2)
-sudo nano /etc/svxlink/svxlink.d/TetraLogic.conf;;
+sudo nano /etc/svxlink/svxlink.conf;;
 3)
-sudo nano /etc/svxlink/tetra_users.json;;
+sudo nano /etc/svxlink/svxlink.d/TetraLogic.conf;;
 4)
+sudo nano /etc/svxlink/tetra_users.json;;
+5)
 sudo systemctl stop svxlink
 wget -q https://raw.githubusercontent.com/HamTetra-CT/users_update/main/tetra_users.json -O /tmp/tetra_users.json
 sudo cp /tmp/tetra_users.json /etc/svxlink/tetra_users.json
 sudo rm /tmp/tetra_users.json
 sudo systemctl start svxlink;;
-5)
-sudo alsamixer;;
 6)
+sudo alsamixer;;
+7)
 sudo systemctl stop svxlink
 cd /home/pi/svxlink/src/build/
 sudo make clean
@@ -59,15 +62,15 @@ sudo git pull
 sudo cmake -DUSE_QT=OFF -DCMAKE_INSTALL_PREFIX=/usr -DSYSCONF_INSTALL_DIR=/etc -DLOCAL_STATE_DIR=/var -DCMAKE_BUILD_TYPE=Release -DWITH_CONTRIB_TETRA_LOGIC=ON ..
 sudo make install
 sudo systemctl start svxlink;;
-7)
-sudo systemctl stop svxlink;;
 8)
-sudo systemctl restart svxlink;;
+sudo systemctl stop svxlink;;
 9)
-sudo raspi-config;;
+sudo systemctl restart svxlink;;
 10)
-sudo reboot;;
+sudo raspi-config;;
 11)
+sudo reboot;;
+12)
 exit 0
 
 esac
