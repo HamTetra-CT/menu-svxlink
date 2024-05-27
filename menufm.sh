40f4118ba09a0f46c0a08604515fccbbc3ec067d
Francisco (F4VSE) <kikosgc@users.noreply.github.com>
2024-05-27 19:42:07 +0200

    Adds new functionalities

--- menufm.sh
+++ menufm.sh
@@ -1,23 +1,24 @@
 # MENU INTERACTIVO PARA CONTROLAR SVXLINK
 #
 #    BASED ON F8ASB & F5UII WORK
-#  HAMTETRA-CT / CT7AJM F4VSE - APRIL 2023
+#  HAMTETRA-CT / CT7AJM F4VSE + CT2IXP - May 2024
 #
 #!/bin/bash
 
 while : ; do
 
-escolha=$(whiptail --title "Menu de Gestão do Repetidor" --menu " >> Faça a sua escolha: " 25 70 10 \
-1 " Editar a configuração do repetidor (svxlink.conf) " \
-2 " Editar RepertearLogic.tcl " \
-3 " Editar Logic.tcl " \
-4 " Configurações audio (alsamixer) " \
-5 " Actualizadar svxlink /!\RISCO DE INCOMPATIBILIDADES/!\ " \
-6 " Parar svxlink " \
-7 " Reboot svxlink " \
-8 " Configurações da Raspberry Pi (raspi-config) " \
-9 " Reboot da Raspberry Pi " \
-10 " Sair do menu "  3>&1 1>&2 2>&3)
+escolha=$(whiptail --title "Menu de Gestão do Repetidor FM Svxlink " --menu " >> Faça a sua escolha: " 25 80 10 \
+1 " Consultar log SVXLink " \
+2 " Editar a configuração do repetidor (svxlink.conf) " \
+3 " Editar RepeaterLogic.tcl " \
+4 " Editar Logic.tcl " \
+5 " Configurações audio (alsamixer) " \
+6 " Actualizadar svxlink /!\RISCO DE INCOMPATIBILIDADES/!\ " \
+7 " Parar svxlink " \
+8 " Reboot svxlink " \
+9 " Configurações da Raspberry Pi (raspi-config) " \
+10 " Reboot da Raspberry Pi " \
+11 " Sair do menu "  3>&1 1>&2 2>&3)
 
 exitstatus=$?
 
@@ -35,31 +36,32 @@ fi
 ## case = Acção em função da escolha
 
 case $escolha in
-
 1)
-sudo nano /etc/svxlink/svxlink.conf;;
+tail -f /var/log/svxlink;;
 2)
-sudo nano /usr/share/svxlink/events.d/RepeaterLogic.tcl;;
+sudo nano /etc/svxlink/svxlink.conf;;
 3)
-sudo nano /usr/share/svxlink/events.d/Logic.tcl;;
+sudo nano /usr/share/svxlink/events.d/RepeaterLogic.tcl;;
 4)
-sudo alsamixer;;
+sudo nano /usr/share/svxlink/events.d/Logic.tcl;;
 5)
+sudo alsamixer;;
+6)
 systemctl stop svxlink
 cd /home/pi/svxlink/src/build/
 sudo make clean
 sudo git pull
 sudo make install
 sudo systemctl start svxlink;;
-6)
-sudo systemctl stop svxlink;;
 7)
-sudo systemctl restart svxlink;;
+sudo systemctl stop svxlink;;
 8)
-sudo raspi-config;;
+sudo systemctl restart svxlink;;
 9)
-sudo reboot;;
+sudo raspi-config;;
 10)
+sudo reboot;;
+11)
 exit 0
 
 esac
